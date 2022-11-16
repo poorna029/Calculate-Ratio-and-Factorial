299c2e7ecad045d67b906b965596f069e43ae6fc
poorna <poorna029@gmail.com>
2022-11-16 10:54:12 +0000

    files added first time all test cases are passed

---
+++ index.js
@@ -0,0 +1,6 @@
+// const func=(a,b,c)=>{
+//     ratio=require(./factorial/index.js);
+//     print(ratio)
+// }
+factorial = require("./utilities/factorial/index");
+ratio = require("./utilities/ratio/index");
---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,13 @@
+const fac = require("../factorial/index");
+const rat = require("../ratio/index");
+
+const ratioAndFactorial = (a = 3, b = 4, c = 5) => {
+  const ratio = rat(a, b);
+  const factorial = fac(c);
+  //   console.log(factorial);
+  let ob = { ratio: ratio, factorial: factorial };
+  return ob;
+};
+ratioAndFactorial();
+// console.log(re);
+module.exports = ratioAndFactorial;
